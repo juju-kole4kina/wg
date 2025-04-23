7163e59f49d32aa39b6e524d984ae9c0aa521599
Juliya Koltsova <juliyaasking009@gmail.com>
2025-04-23 22:06:12 +0200

    feat: настройка подключения к postgresql

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const pg = require('pg')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const { Pool } = pg
+
+const pool =  new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+})
+
+module.exports = { pool }
